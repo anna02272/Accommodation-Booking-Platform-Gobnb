aac064d913a91ef04029a261f2c8f65c41724d57
LAPTOP-SC9RDVNE\domon <domonjianna@gmail.com>
2023-11-27 01:20:09 +0100

    Validation for reset password and Redirect Guard

--- Airbnb_frontend/src/app/components/reset-password/reset-password.component.ts
+++ Airbnb_frontend/src/app/components/reset-password/reset-password.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {  ActivatedRoute, Router } from '@angular/router';
 import { AuthService } from 'src/app/services';
-import { Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-reset-password',
@@ -11,30 +11,39 @@ import { Validators } from '@angular/forms';
 export class ResetPasswordComponent implements OnInit{
   notification: any = { msgType: '', msgBody: '' };
   email!: string
-  passwordResetToken: string = '';
-  password: string = '';
-  passwordConfirm: string = '';
+  passwordForm!: FormGroup;
 
   constructor(
     private authService: AuthService, 
     private router: Router,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private formBuilder: FormBuilder
     ) {
   }
   ngOnInit() {
     this.route.queryParams.subscribe(params => {
       this.email = params['email'];
     });
-   
+    this.passwordForm = this.formBuilder.group({
+      passwordResetToken: ['', Validators.required],
+      password: ['', [Validators.required, this.passwordValidator()]],
+      passwordConfirm: ['', Validators.required],
+    });
   }
 
   resetPassword() {
-    if (this.password !== this.passwordConfirm) {
+    if (this.passwordForm.invalid) {
+      this.notification = { msgType: 'error', msgBody: 'Please fix the form errors.' };
+      return;
+    }
+
+    const { password, passwordConfirm, passwordResetToken } = this.passwordForm.value;
+    if (password !== passwordConfirm) {
       this.notification = { msgType: 'error', msgBody: 'Password and password confirm do not match.' };
       return;
     }
 
-    this.authService.resetPassword(this.passwordResetToken, this.password, this.passwordConfirm).subscribe(
+    this.authService.resetPassword(passwordResetToken, password, passwordConfirm).subscribe(
       () => {
         this.notification = { msgType: 'success', msgBody: 'Password reset successful.' };
        console.log('Password reset successful');
---
+++ Airbnb_frontend/src/app/services/redirect.guard.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RedirectGuard implements CanActivate {
+
+  constructor(private router: Router) {}
+
+  canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+
+    const isRedirected = localStorage.getItem('isRedirected');
+
+    if (isRedirected) {
+      return true;
+    } else {
+      this.router.navigate(['/home']);
+      return false;
+    }
+  }
+}
