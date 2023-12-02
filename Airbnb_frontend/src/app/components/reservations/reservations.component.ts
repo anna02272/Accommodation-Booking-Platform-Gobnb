33af6efeb49b17ef6df5035a81a50aaabd758f5b
LAPTOP-SC9RDVNE\domon <domonjianna@gmail.com>
2023-12-02 13:03:24 +0100

    Cancel reservation

--- Airbnb_frontend/src/app/components/reservations/reservations.component.ts
+++ Airbnb_frontend/src/app/components/reservations/reservations.component.ts
@@ -1,12 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { GetReservation } from 'src/app/models/GetReservation';
+import { RefreshService } from 'src/app/services/refresh.service';
+import { ReservationService } from 'src/app/services/reservation.service';
 
 @Component({
   selector: 'app-reservations',
   templateUrl: './reservations.component.html',
   styleUrls: ['./reservations.component.css']
 })
-export class ReservationsComponent {
+export class ReservationsComponent implements OnInit{
+reservations: GetReservation[] = [];
+notification = { msgType: '', msgBody: '' };
 
-  
-
+constructor(
+  private resService: ReservationService,
+  private refreshService: RefreshService,
+) {}
+ngOnInit() {
+  this.load();
+  this.subscribeToRefresh();
+}
+load() {
+  this.resService.getAll().subscribe((data: GetReservation[]) => {
+    this.reservations = data;
+});
+}
+private subscribeToRefresh() {
+  this.refreshService.getRefreshObservable().subscribe(() => {
+    this.load();
+  });
+}
+cancelReservation(id: string): void {
+  this.resService.cancelReservation(id).subscribe(
+    () => {
+      this.refreshService.refresh();
+      this.notification = { msgType: 'success', msgBody: `Reservation canceled successfully.` };
+    },
+    (error) => {
+      this.notification = { msgType: 'error', msgBody: `Cannot cancel reservation, check-in date has already started` };
+      console.error('Error canceling reservation:', error);
+    }
+  );
+}
 }
--- Airbnb_frontend/src/app/models/reservation.ts
+++ Airbnb_frontend/src/app/models/reservation.ts
@@ -3,11 +3,13 @@ export class Reservation {
     accommodation_id: string;
     check_in_date!: string;
     check_out_date: string;
+    number_of_guests: number;
 
-     constructor(accommodation_id: string,check_in_date: any, check_out_date: any) {
+     constructor(accommodation_id: string,check_in_date: any, check_out_date: any, number_of_guests: number) {
       this.accommodation_id = accommodation_id;
       this.check_in_date = check_in_date,
       this.check_out_date = check_out_date
+      this.number_of_guests = number_of_guests
 
     }
 
