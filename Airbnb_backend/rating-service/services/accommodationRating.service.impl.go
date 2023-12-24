3d1eed3f282dbb1dd8fcba0cbc0c5d01f09a1717
Tina <valentinagabricc2@gmail.com>
2023-12-24 21:15:07 +0100

    Rating accommodation

--- Airbnb_backend/rating-service/services/accommodationRating.service.impl.go
+++ Airbnb_backend/rating-service/services/accommodationRating.service.impl.go
@@ -22,7 +22,7 @@ func (s *AccommodationRatingServiceImpl) SaveRating(rating *domain.RateAccommoda
 		"accommodationID": rating.Accommodation,
 		"guest._id":       rating.Guest.ID,
 	}
-
+	
 	existingRating := &domain.RateAccommodation{}
 	err := s.collection.FindOne(context.Background(), filter).Decode(existingRating)
 
